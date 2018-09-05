b1614cc1559fcf4248d488f4fcf3d52cc69a40e4
Kyle <39253934+JaegerCryptic@users.noreply.github.com>
2018-09-06 09:04:34 +1000

    Save feature complete
    Save feature works as intended.

--- CarolinesClassroomRobot/ClassroomRobot/ClassroomRobot/Saving.cs
+++ CarolinesClassroomRobot/ClassroomRobot/ClassroomRobot/Saving.cs
@@ -17,13 +17,29 @@ namespace ClassroomRobot
             foreach(Students student in GridPosition.StudentList)
             {
                 var studentNames = student.Names;
+
+                if (studentNames == null)
+                {
+                    studentNames = "Empty";
+                }
+
                 var studentRow = student.Row;
                 var studentColumn = student.Column;
-                var csvFormat = string.Format("{0},{1},{2}", studentColumn, studentRow, studentNames);
-                csv.AppendLine(csvFormat);
+                var csvStudentFormat = string.Format("{0},{1},{2}", studentColumn, studentRow, studentNames);
+                csv.AppendLine(csvStudentFormat);
+            }
+
+            foreach(Colours colour in GridPosition.itemlist)
+            {
+                var colourColumn = colour.Column;
+                var colourRow = colour.Row;
+                var myColour = colour.Colour;
+
+                var csvColourFormat = string.Format("{0},{1},{2},{3}", colourColumn, colourRow, "BKGRND FILL", myColour);
+                csv.AppendLine(csvColourFormat);
             }
             File.WriteAllText("UpdatedClassRoomLayout.csv", csv.ToString());
-
+            MessageBox.Show("New layout successfully saved as UpdatedClassRoomLayout.csv.");
         }
 
     }
